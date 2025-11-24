7379308eb17dce105e5d0397b8a6c387e74a60b7
Anastasia <filippov31012007@gmail.com>
2025-11-24 14:28:03 +0500

    Разработанное главное окно. Osennikov

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace PrintManagementSystem_Osennikov
         {
             InitializeComponent();
         }
+
+        private void DeleteOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectedType(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SelectedFormat(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void textBoxCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void AddOperation(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ColorsChange(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
